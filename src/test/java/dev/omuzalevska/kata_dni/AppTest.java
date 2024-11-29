abe1f5835d798796ffca0a618d247cbb43ad3dcf
OMuzalevska <oksana.muzalevskaja@gmail.com>
2024-11-29 16:03:42 +0100

    first commit

---
+++ src/test/java/dev/omuzalevska/kata_dni/AppTest.java
@@ -0,0 +1,18 @@
+package dev.omuzalevska.kata_dni;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
